79e59080989e16acd8467b65777f8f602f29c71b
VISHALSHARMAVS <123116091+VISHALSHARMAVS@users.noreply.github.com>
2023-11-18 14:04:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+let signIn;
+let signUp;
+
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementsById('container');
+
+signUpButton.addEventListener('click', () => {
+    container.classList.add("right-panel-active");
+});
+
+signInButton.addEventListener('click', () => {
+    container.classList.remove("right-panel-active");
+});
